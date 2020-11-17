ba95de70acbf8b65e30b854bf8ac24f30504d010
famer <famerimi@gmail.com>
2020-11-17 13:29:56 +0100

    idée n°1

--- squelette1FRog/src/frog/Frog.java
+++ squelette1FRog/src/frog/Frog.java
@@ -2,11 +2,42 @@ package frog;
 
 import gameCommons.Game;
 import gameCommons.IFrog;
+import util.Case;
+import util.Direction;
 
 public class Frog implements IFrog {
-	// hello my name is nnao
-	//test0
 	private Game game;
+	private Case pos;
+	private Direction dir;
+
+	//constructeur
+	public Frog(){
+
+	}
+
+
+	/**
+	 * Donne la position actuelle de la grenouille
+	 * @return
+	 */
+	public Case getPosition(){
+		return this.pos;
+
+	}
+	/**
+	 * Donne la direction de la grenouille, c'est � dire de son dernier mouvement
+	 * @return
+	 */
+	public Direction getDirection(){
+		return this.dir;
+	}
+	/**
+	 * D�place la grenouille dans la direction donn�e et teste la fin de partie
+	 * @param key
+	 */
+	public void move(Direction key){
+
+	}
 
 
 }
